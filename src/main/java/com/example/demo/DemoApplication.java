34e532f9ea08f5f368034c5672f4009de09af6d0
ChangHyukGod <117065614+ChangHyukGod@users.noreply.github.com>
2022-11-27 17:04:05 +0900

    map

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication extends SpringBootServletInitializer {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
@@ -17,5 +16,28 @@ public class DemoApplication extends SpringBootServletInitializer {
 	@RequestMapping("/")
 	String sayHello() {
 		return "Hello Azure Hi" ;
+	}
+	
+	@RequestMapping("/")
+	string mapview(){
+		return <html>
+	<head>
+	<meta charset="utf-8"/>
+	<title>Kakao 지도 시작하기</title>
+	</head>
+	<body>
+	<div id="map" style="width:500px;height:400px;"></div>
+	<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=발급받은 APP KEY를 넣으시면 됩니다."></script>
+	<script>
+		var container = document.getElementById('map');
+		var options = {
+			center: new kakao.maps.LatLng(33.450701, 126.570667),
+			level: 3
+		};
+
+		var map = new kakao.maps.Map(container, options);
+	</script>
+	</body>
+	</html>
 	}
 }
